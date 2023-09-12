1e208ea5e02f08d25d5834e5a7a3f3c8cd22b206
gimgihyeon <gihyeon6394@gmail.com>
2023-09-12 19:16:22 +0900

    feat : RxJava dependency 추가, 예제 코드

--- src/main/java/org/example/part5/reactive/reportemp/Client.java
+++ src/main/java/org/example/part5/reactive/reportemp/Client.java
@@ -1,12 +1,22 @@
 package org.example.part5.reactive.reportemp;
 
+import io.reactivex.rxjava3.core.Observable;
+
 import java.util.concurrent.Flow;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
 
     public static void main(String[] args) {
-        getTemperatures("Seoul") // Publisher 생성
-                .subscribe(new TempSubscriber()); // Subscriber 등록
+//        getTemperatures("Seoul") // Publisher 생성
+//                .subscribe(new TempSubscriber()); // Subscriber 등록
+
+        // RxJava
+        Observable<Long> onePerSec = Observable.interval(1, TimeUnit.SECONDS);
+//        onePerSec.subscribe(i -> System.out.println(TempInfo.fetch( "New York" )));
+        onePerSec.blockingSubscribe(
+                i -> System.out.println(TempInfo.fetch("New York"))
+        );
     }
 
     public static Flow.Publisher<TempInfo> getTemperatures(String town) {
